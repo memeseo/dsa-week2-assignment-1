8ba598ba33c48c0d897c3de7c2b7d77b706a96ee
hannut91 <hannut91@gmail.com>
2023-09-11 13:09:54 +0900

    Revert "Merge pull request #4 from changuri/main"
    This reverts commit c446b59863c34a5871d07ba3c5af11853d6bcc30, reversing
    changes made to 1027fe451e9df5c6f1674131c39f37bb551c897e.

--- problem-1-1/problem-1-1.test.js
+++ problem-1-1/problem-1-1.test.js
@@ -1,49 +1,5 @@
-// D, S, A, E, X, A, M, P, L, E
 class Bag {
-  #items = [];
-
-  add(item) {
-    this.#items.push(item);
-  }
-
-  isEmpty() {
-    return this.#items.length === 0;
-  }
-
-  size() {
-    return this.#items.length;
-  }
-
-  display() {
-    console.log(this.#items);
-  }
-
-  [Symbol.iterator]() {
-    let index = 0;
-    const data = this.#items;
-
-    return {
-      next() {
-        return index < data.length ? {
-          done: false, value: data[index++],
-        } : { done: true };
-      },
-    };
-  }
 }
-
-// const bag = new Bag();
-// bag.add('D');
-// bag.add('S');
-// bag.add('A');
-// bag.add('E');
-// bag.add('X');
-// bag.add('A');
-// bag.add('M');
-// bag.add('P');
-// bag.add('L');
-// bag.add('E');
-// bag.display();
 
 test('백은 비어있는 상태로 생성된다', () => {
   const bag = new Bag();
--- problem-1-2/problem-1-2.test.js
+++ problem-1-2/problem-1-2.test.js
@@ -1,52 +1,5 @@
-class Bag {
-  #items = [];
-
-  add(item) {
-    this.#items.push(item);
-  }
-
-  isEmpty() {
-    return this.#items.length === 0;
-  }
-
-  size() {
-    return this.#items.length;
-  }
-
-  display() {
-    console.log(this.#items);
-  }
-
-  [Symbol.iterator]() {
-    let index = 0;
-    const data = this.#items;
-
-    return {
-      next() {
-        return index < data.length ? {
-          done: false, value: data[index++],
-        } : { done: true };
-      },
-    };
-  }
-}
-
 const solution = (numbers) => {
-  const bag = new Bag();
-  numbers.forEach((num) => {
-    bag.add(num);
-  });
-
-  let sum = 0;
-  for (const num of bag) {
-    sum += num;
-  }
-  // console.log('display', bag.display());
-
-  return Math.floor(sum / bag.size());
 };
-// const scoresData = [1, 3, 5, 7, 9, 11];
-// console.log(solution(scoresData));
 
 test('숫자 배열의 평균을 반환한다', () => {
   expect(solution([1, 2, 3, 4, 5])).toEqual(3);
