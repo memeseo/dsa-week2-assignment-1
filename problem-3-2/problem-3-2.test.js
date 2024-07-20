7ceecf1ebbd314f747ff2bc14a89c57bc4d6ca91
umbrellamoss <umbrellamoss@gmail.com>
2024-07-20 12:52:29 +0900

    Feat. problem-3-2

--- problem-3-2/problem-3-2.test.js
+++ problem-3-2/problem-3-2.test.js
@@ -1,4 +1,62 @@
+class Queue {
+  constructor(){
+    this.items = [];
+  }
+
+  isEmpty(){
+    return this.items.length === 0;
+  }
+
+  size(){
+    return this.items.length;
+  }
+
+  enqueue(item){
+    this.items.push(item);
+  }
+
+  dequeue(){
+    if(this.size() === 0) throw new Error('큐가 비어있습니다');
+
+    const item = this.items.shift();
+    return item;
+  }
+
+  setNumbers(number){
+    for(let i = 1; i <= number; i++){
+      this.enqueue(i);
+    }
+  }
+
+  [Symbol.iterator](){
+    let index = 0;
+    const queue = [...this.items];
+
+    return {
+      next() {
+        return index < queue.length
+          ? { done: false, value: queue[index++] }
+          : { done: true };
+      },
+    };
+  }
+
+
+}
+
 const solution = (N, M) => {
+  const queue = new Queue();
+  queue.setNumbers(N);
+
+  while (queue.size() > 1) {
+    for (let index = 0; index < M - 1; index += 1) {
+      const dequeuedNumber = queue.dequeue();
+      queue.enqueue(dequeuedNumber);
+    }
+    queue.dequeue();
+  }
+
+  return queue.dequeue();
 };
 
 test('N명의 사람이 있을 때 M번째 사람을 없앨 때 마지막에 죽는 사람의 순서를 반환한다', () => {
