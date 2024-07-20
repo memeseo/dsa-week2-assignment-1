0b1a3cf5f6ccc12b5e66a84f3816460f31743a27
umbrellamoss <umbrellamoss@gmail.com>
2024-07-20 11:21:54 +0900

    Feat. problem-2-1

--- problem-2-1/problem-2-1.test.js
+++ problem-2-1/problem-2-1.test.js
@@ -1,4 +1,47 @@
+// 1. 스택의 정의에 따라서 API를 설계해 주세요.
+
+// | 함수 시그니처 | 설명 |
+// | ----------- | ----------- | ----------- | ----------- |
+// | isEmpty(): boolean | stack 배열의 빈값 여부에 따라 boolean 타입을 반환합니다. |
+// | push(item): void | stack 배열에 아이템을 추가합니다. |
+// | size(): number | stack 배열의 길이를 반환합니다. |
+// | pop(): string | Error | stack 배열에 가장 최근에 삽입한 값을 반환합니다. |
+
 class Stack {
+  constructor(){
+    this.stack = [];
+  }
+
+  isEmpty(){
+    return this.stack.length === 0;
+  }
+
+  push(item){
+    this.stack.push(item);
+  }
+
+  size(){
+    return this.stack.length;
+  }
+
+  pop(){
+    if(this.isEmpty()) throw new Error('스택이 비어있습니다');
+
+    return this.stack.pop();
+  }
+
+  [Symbol.iterator](){
+    let index = 0,
+        stack = [...this.stack];
+        stack = stack.reverse();
+
+    return {
+      next(){
+        return index < stack.length ? {done : false, value : stack[index++]} : {done : true}
+      }
+    }
+
+  }
 }
 
 test('스택을 생성하면 비어있다', () => {
